4578fa08798b4fcdb31915c2dde7c7e55cc34e3f
Md-JahidHasan <md.jahidhasan.seu@gmail.com>
2023-04-03 21:43:35 +0600

    Card design change

--- src/pages/Home/newProduct/NewProduct.js
+++ src/pages/Home/newProduct/NewProduct.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { RiHeartLine } from 'react-icons/ri';
+import { TbCurrencyTaka } from 'react-icons/tb';
 
 const NewProduct = ({product}) => {
     return (
-        <Link to={`/products/${product._id}`}>
+        <Link className='shadow-lg hover:shadow-sm  rounded-sm' to={`/products/${product._id}`}>
             <div>
-                <img src={product.image} alt="" />
+                <img className='' src={product.image} alt="" />
             </div>
-            <div>
+            <div className='flex justify-between p-2'>
                 <div>
-                    <h1>{product.name}</h1>
-                    <p>{product.price}</p>
+                    <h1 className='text-xl font-bold'>{product.name}</h1>
+                    <p className='flex items-center'><TbCurrencyTaka></TbCurrencyTaka>{product.price}</p>
                 </div>
                 <div>
-                    
+                    <RiHeartLine></RiHeartLine>
                 </div>
             </div>
+            <button className='bg-yellow-400 w-full p-2'>ADD TO CART</button>
         </Link>
     );
 };
